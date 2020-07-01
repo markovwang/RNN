62191a015d3a84f5ad5a1d21ea34e0595abb0aca
markovwang <61182000+markovwang@users.noreply.github.com>
2020-07-02 00:08:43 +0800

    update files

--- utils.py
+++ utils.py
@@ -7,3 +7,21 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
+def load_training_data(path='training_label.txt'):
+    if 'training_label' in path:
+        with open(path, 'r') as f:
+            lines = f.readlines()
+            lines = [line.strip('\n').split(' ') for line in lines]
+        x = [line[2:] for line in lines]
+        y = [line[0] for line in lines]
+        return x, y
+    else:
+        with open(path, 'r') as f:
+            lines = f.readlines()
+            x = [line.strip('\n').split(' ') for line in lines]
+        return x
+
+
+if __name__ == '__main__':
+    x, y = load_training_data()
+    print(x)
