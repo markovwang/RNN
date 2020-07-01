7b84e1ce28dc345a25bb90bf7f4966d13b2d3396
markovwang <61182000+markovwang@users.noreply.github.com>
2020-07-02 01:05:52 +0800

    update utlis

--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
 # utils.py
-# first we define some useful functions
+# useful functions
+# don't stop a dannchouuuuuuuuu
+# ₘₙⁿ
+# ▏n
+# █▏　､⺍
+# █▏ ⺰ʷʷｨ
+# █◣▄██◣
+# ◥██████▋
+# 　◥████ █▎
+# 　　███▉ █▎
+# 　◢████◣⌠ₘ℩
+# 　　██◥█◣\≫
+# 　　██　◥█◣
+# 　　█▉　　█▊
+# 　　█▊　　█▊
+# 　　█▊　　█▋
+# 　　 █▏　　█▙
+# 　　 █
 import torch
 import numpy as np
 import pandas as pd
@@ -9,19 +26,35 @@ import torch.nn.functional as F
 
 def load_training_data(path='training_label.txt'):
     if 'training_label' in path:
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             lines = [line.strip('\n').split(' ') for line in lines]
         x = [line[2:] for line in lines]
         y = [line[0] for line in lines]
         return x, y
     else:
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             x = [line.strip('\n').split(' ') for line in lines]
         return x
 
 
+def load_testing_data(path='testing_data.txt'):
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        X = ["".join(line.strip('\n').split(",")[1:]).strip() for line in lines[1:]]
+        # X = [sen.split(' ') for sen in X]
+    return X
+
+
+def evaluation(outputs, labels):
+    # if out > 0.5 means negtive
+    outputs[outputs > 0.5] = 1
+    outputs[outputs < 0.5] = 0
+    correct = torch.sum(torch.eq(outputs, labels)).item()
+    return correct
+
+
 if __name__ == '__main__':
-    x, y = load_training_data()
-    print(x)
+    x = load_testing_data()
+    print(x[0])
